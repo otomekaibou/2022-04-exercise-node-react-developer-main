351437f5a3367a16679df35e1f49b7a36ce0c103
otomekaibou <71198204+otomekaibou@users.noreply.github.com>
2022-06-28 23:38:21 -0700

    removed cors error access control

---
+++ web/src/components/repositoryContainer.tsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+
+export function RepositoryContainer() {
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [repoData, setRepoData] = useState([]);
+
+  useEffect(() => {
+    let mounted = true;
+    fetch("http://localhost:4000/repos", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        'Access-Control-Allow-Origin': 'http://localhost:3000',
+      }
+    },)
+    .then((res) => res.json())
+    .then(
+      (json) => {
+        if (mounted) {
+          setRepoData(json);
+          console.log(json);
+          setLoading(false);
+        }
+      },
+      (error) => {
+        setError(error);
+        setLoading(false);
+      }
+    );
+    mounted = false;
+    return;
+  }, []);
+
+  if (loading) {
+    return <h2 className="status-message">Retrieving data...</h2>
+  } else if (error) {
+    return <div className="status-message">{error}</div>
+  } else if (repoData.length === 0) {
+    return <h2 className="status-message">No data</h2>
+  } else {
+
+    return (
+      <React.Fragment>
+        <div>hullo</div>
+      </React.Fragment>
+    );
+  }
+}
