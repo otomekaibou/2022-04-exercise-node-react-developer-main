c5e5e8d82e4daa30516c185e5f6fadd44d7b3af6
otomekaibou <71198204+otomekaibou@users.noreply.github.com>
2022-06-28 22:51:46 -0700

    try to fetch data for api

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,20 +1,28 @@
 import { Router, Request, Response } from 'express';
 import fs from 'fs';
+const fetch = require('node-fetch');
 
 export const repos = Router();
+const JSON_URL = "https://api.github.com/users/silverorange/repos";
 
 repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
 
   res.status(200);
 
-  let fileData = JSON.parse(fs.readFileSync("data/repos.json", "utf8"))
-
   try {
-    let fileData = JSON.parse(fs.readFileSync("data/repos.json", "utf8"))
+    let fileData = JSON.parse(fs.readFileSync("data/repos.json", "utf8"));
     res.json(fileData);
   } catch (error) {
     console.log(error);
     res.status(400).send(error);
   }
+
+  // try {
+  //   let urlData = await fetch(JSON_URL)
+  //   console.log(urlData)
+  // } catch (error) {
+  //   console.log(error);
+  // }
+  
 });
